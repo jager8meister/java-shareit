3e20da9055706616bc2a4201cf81c4513bb7eda1
danil/// <d@d.com>
2023-02-18 20:33:20 +0300

    whitespace in UserDaoImpl

--- src/main/java/ru/practicum/shareit/user/dao/UserDaoImpl.java
+++ src/main/java/ru/practicum/shareit/user/dao/UserDaoImpl.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class UserDaoImpl implements UserDao {
     private long id;
     private Map<Long, User> userMap = new HashMap<>();
+
     @Override
     public List<User> findAll() {
         return new ArrayList<>(userMap.values());
