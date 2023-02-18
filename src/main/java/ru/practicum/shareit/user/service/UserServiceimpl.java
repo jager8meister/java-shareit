8c7cfe0763149b4de3435fe7c32e8ddcd7b3f11f
danil/// <d@d.com>
2023-02-18 16:54:02 +0300

    more small refactoring

--- src/main/java/ru/practicum/shareit/user/service/UserServiceimpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceimpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class UserServiceimpl implements UserService{
+public class UserServiceimpl implements UserService {
     private final UserDao userDao;
 
     @Override
